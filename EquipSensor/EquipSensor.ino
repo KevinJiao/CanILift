4570d2ac91d41b89147cee8cefffc84097279a20
Kevin Jiao <kevin.jiao@berkeley.edu>
2015-03-01 13:17:01 -0800

    added sampling

--- EquipSensor/EquipSensor.ino
+++ EquipSensor/EquipSensor.ino
@@ -2,6 +2,7 @@
 
 const int signalpin = 3;
 const int inputpin = 5;
+const int threshold = 1000;
 
 void setup(){
     pinMode(signalpin, OUTPUT);
@@ -10,21 +11,36 @@ void setup(){
 }
 
 void loop(){
-    int duration = polldistance();
-    Serial.println(duration);
+    boolean occupied = check_occupied(10);
+    if(occupied){
+      Serial.println("Equipment is in use");
+    } else{
+      Serial.println("Equipment is free");
+    }
     delay(1000);
 
 
 
 }
 
-int polldistance(){
+int poll_distance(){
     digitalWrite(signalpin, LOW); //pulses output HIGH for 5 microseconds
     delayMicroseconds(2);
     digitalWrite(signalpin, HIGH);
     delayMicroseconds(5);
     digitalWrite(signalpin, LOW);
 
-    int duration = pulseIn(inputpin, HIGH);
+    int duration = pulseIn(inputpin, HIGH);  
+    delay(100);
     return duration;
+}
+
+boolean check_occupied(int num_samples){
+  int counter =0; //counts number of times poll_distance returns occupied
+  for (int i = 0; i < num_samples; i++){
+    if (poll_distance()<threshold){
+      counter++;
+    }
+  }
+  return ((counter/num_samples) > 0.5);
 }
